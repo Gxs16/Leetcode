6f98b0349381fcc0894fbce9a7754b88ecbed0bd
X.Guo <847486005@qq.com>
2021-12-13 10:14:43 +0800

    0037_Solve_Sudoku

--- src/Python/0037_Solve_Sudoku.py
+++ src/Python/0037_Solve_Sudoku.py
@@ -3,59 +3,43 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        column_dict = {}
-        row_dict = {}
-        block_dict = {}
+        from collections import defaultdict
+        column_dict = defaultdict(list)
+        row_dict = defaultdict(list)
+        block_dict = defaultdict(list)
         for i in range(9):
             for j in range(9):
+                block = (i//3, j//3)
+
                 if board[i][j] != '.':
-                    if i in row_dict:
-                        row_dict[i].append(board[i][j])
-                    else:
-                        row_dict[i] = [board[i][j]]
+                    row_dict[i].append(board[i][j])
+                    column_dict[j].append(board[i][j])
+                    block_dict[block].append(board[i][j])
 
-                    if j in column_dict:
-                        column_dict[j].append(board[i][j])
-                    else:
-                        column_dict[j] = [board[i][j]]
+        self.solveUnit(board, row_dict, column_dict, block_dict, 0)
 
-                    block = (i//3, j//3)
-                    if block in block_dict:
-                        block_dict[block].append(board[i][j])
-                    else:
-                        block_dict[block] = [board[i][j]]
-
-        self.solveUnit(board, row_dict, column_dict, block_dict, 0, 0)
-
-    def getStart(self, board, row_start, column_start):
-        for i in range(row_start, 9):
-            for j in range(column_start, 9):
-                if board[i][j] == '.':
-                    return i, j
-        return None, None
-
-    def solveUnit(self, board, row_dict, column_dict, block_dict, row_start, column_start):
-        i, j = self.getStart(board, row_start, column_start)
-        if i == None:
+    def solveUnit(self, board, row_dict, column_dict, block_dict, start):
+        for k in range(start, 81):
+            i = k//9
+            j = k%9
+            if board[i][j] == '.':
+                start = k+1
+                break
+        else:
             return True
         block = (i//3, j//3)
-        non_available = row_dict[i]+column_dict[j]+block_dict[block]
-        available = []
-        for num in range(1, 10):
-            if not str(num) in non_available:
-                available.append(str(num))
-        if available:
-            board_next = board.copy()
-            for det in available:
-                row_dict_next = row_dict.copy()
-                column_dict_next = column_dict.copy()
-                block_dict_next = block_dict.copy()
-                row_dict_next[i].append(det)
-                column_dict_next[j].append(det)
-                block_dict_next[block].append(det)
-                board_next[i][j] = det
-                if self.solveUnit(board_next, row_dict_next, column_dict_next, block_dict_next, i, j):
-                    board = board_next
+        non_available = set(row_dict[i]+column_dict[j]+block_dict[block])
+
+        for det in [str(num) for num in range(1, 10)]:
+            if not det in non_available:
+                row_dict[i].append(det)
+                column_dict[j].append(det)
+                block_dict[block].append(det)
+                
+                if self.solveUnit(board, row_dict, column_dict, block_dict, start):
+                    board[i][j] = det
                     return True
-        else:
-            return False
+                else:
+                    row_dict[i].pop(-1)
+                    column_dict[j].pop(-1)
+                    block_dict[block].pop(-1)
