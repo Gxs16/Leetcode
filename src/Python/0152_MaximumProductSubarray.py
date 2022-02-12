b5fd68893b5a3a6c26f8ac1ee276caf31c4f2347
X.Guo <847486005@qq.com>
2022-02-13 00:18:32 +0800

    0152

---
+++ src/Python/0152_MaximumProductSubarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_res = [1]*len(nums)
+        min_res = [1]*len(nums)
+        for i in range(len(nums)):
+            num = nums[i]
+            if num >= 0:
+                max_res[i] = max(num, max_res[i-1]*num)
+                min_res[i] = min(num, min_res[i-1]*num)
+            else:
+                max_res[i] = max(num, min_res[i-1]*num)
+                min_res[i] = min(num, max_res[i-1]*num)
+        return max(max_res)
