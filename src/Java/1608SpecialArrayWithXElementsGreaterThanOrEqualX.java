66a14b5e93764b11349f9d841a0c8f487c150d9c
X.Guo <847486005@qq.com>
2022-09-12 16:35:42 +0800

    2022年9月12日

---
+++ src/Java/1608SpecialArrayWithXElementsGreaterThanOrEqualX.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= nums.length-i) {
+                if (i > 0) {
+                    if (nums[i-1] < nums.length-i) {
+                        return nums.length-i;
+                    }
+                } else {
+                    return nums.length-i;
+                }
+            }
+        }
+        return -1;
+    }
+}
