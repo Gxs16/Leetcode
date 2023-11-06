0c05c906a1a01d2751d0433eef422b88260cecce
Gxs16 <847486005@qq.com>
2023-11-06 23:53:02 +0800

    29. Divide Two Integers

---
+++ src/Java/Solution0029.java
@@ -0,0 +1,44 @@
+public class Solution0029 {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        boolean reverse = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            reverse = !reverse;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            reverse = !reverse;
+        }
+        if (dividend > divisor) return 0;
+        int ans = 1;
+        int divisorCopy = divisor;
+        while (dividend - divisorCopy <= divisorCopy) {
+            ans += ans;
+            divisorCopy += divisorCopy;
+        }
+        if (reverse) {
+            return -ans - divide(dividend - divisorCopy, divisor);
+        }
+        return ans + divide(dividend - divisorCopy, divisor);
+    }
+
+
+    public static void main(String[] args) {
+        Solution0029 solution = new Solution0029();
+//        System.out.println(multiply(157, (int) Math.pow(2, 15)));
+        System.out.println(solution.divide(1100540749, -1090366779));
+//        System.out.println(-5<<1);
+    }
+}
